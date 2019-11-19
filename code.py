acf01c8729a46318951ad5033012f9a5d032d8be
Starship87 <55468638+Starship87@users.noreply.github.com>
2019-11-18 18:55:53 -0600

    Add files via upload

--- code.py
+++ code.py
@@ -3,51 +3,48 @@ import time
 import random
 colors = [(255,0,0), (0,0,255), (0,255,0),(244, 252, 3), (206, 3, 252)]
 r =(random.randint(0, len(colors)-1))
-
+s =(random.randint(0, len(colors)-1))
+q =(random.randint(0, len(colors)-1))
+a =(random.randint(0, len(colors)-1))
+color = colors[r]
+print (color)
 while True:
     if cpx.touch_A1:
         cpx.stop_tone()
-    if cpx.touch_A2:
-        cpx.play_tone(250,1)
-        cpx.play_tone(300,1)
-        cpx.play_tone(350,1)
-        cpx.play_tone(400,1)
-        cpx.play_tone(450,1)
-        cpx.play_tone(450,1)
-        cpx.play_tone(400,1)
-        cpx.play_tone(350,1)
-        cpx.play_tone(300,1)
-        cpx.play_tone(250,1)
-    if cpx.touch_A3:
-       cpx.pixels [0] = colors[0]
-       cpx.pixels [9] = colors[0]
-       cpx.pixels.brightness = 0.1
-       cpx.pixels.show()
-    if cpx.touch_A4:
-       cpx.pixels [1] = colors[1]
-       cpx.pixels [8] = colors[1]
-       cpx.pixels.brightness = 0.1
-       cpx.pixels.show()
-    if cpx.touch_A5:
-       cpx.pixels [2] = colors[2]
-       cpx.pixels [7] = colors[2]
-       cpx.pixels.brightness = 0.1
-       cpx.pixels.show()
-    if cpx.touch_A6:
-       cpx.pixels [3] = colors[3]
-       cpx.pixels [6] = colors[3]
-       cpx.pixels.brightness = 0.1
-       cpx.pixels.show()
-    if cpx.touch_A7:
-       cpx.pixels [4] = colors[4]
-       cpx.pixels [5] = colors[4]
+    #if cpx.touch_A2:
+        #cpx.play_tone(300,1)
+        #cpx.play_tone(350,1)
+        #cpx.play_tone(400,1)
+        #cpx.play_tone(450,1)
+        #cpx.play_tone(450,1)
+        #cpx.play_tone(400,1)
+        #cpx.play_tone(350,1)
+        #cpx.play_tone(300,1)
+        #cpx.play_tone(250,1)
+    if cpx.button_a:
+       cpx.pixels [r] = colors[r]
+       time.sleep(2)
+       cpx.pixels.fill((0, 0,0))
+       time.sleep(0.5)
+       cpx.pixels [s] = colors[s]
+       time.sleep(2)
+       cpx.pixels.fill((0, 0,0))
+       time.sleep(0.5)
+       cpx.pixels [q] = colors[q]
+       time.sleep(2)
+       cpx.pixels.fill((0, 0,0))
+       time.sleep(0.5)
+       cpx.pixels [a] = colors[a]
+       time.sleep(2)
+       cpx.pixels.fill((0, 0,0))
+       time.sleep(0.5)
+
        cpx.pixels.brightness = 0.1
        cpx.pixels.show()
     if cpx.button_b:
        cpx.pixels.fill((0, 0,0))
        cpx.pixels.brightness = 0
        cpx.pixels.show()
-
 
 
 
